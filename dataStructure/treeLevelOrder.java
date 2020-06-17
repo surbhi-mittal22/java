8f2aaef537ef12dee19ff5d453775464f021b089
Surbhi Mittal <65413535+surbhi-mittal22@users.noreply.github.com>
2020-06-17 18:18:28 +0530

    Add files via upload

---
+++ dataStructure/treeLevelOrder.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/* 
+    
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static void levelOrder(Node root) {
+         Queue<Node> Q = new LinkedList<Node>();
+       Q.add(root);
+       
+       while(!Q.isEmpty()) {
+           Node n = Q.poll();
+           System.out.print(n.data + " ");
+           if(n.left != null) Q.add(n.left);
+           if(n.right != null) Q.add(n.right);
+      
+       }
+       
+      
+      
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }	
+}
